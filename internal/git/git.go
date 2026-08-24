aac758b5f51fa7d177f51cf647d958fc34439feb
Keshav Bansal <keshavbansal9a@gmail.com>
2026-08-24 20:50:43 +0530

    [refactor] Simplified the code

--- internal/git/git.go
+++ internal/git/git.go
@@ -8,13 +8,11 @@ import (
 )
 
 func (r *Repo) ExecGitCommand(ctx context.Context, gitPath string, args ...string) ([]byte, error) {
-	args = append([]string{"-c", "core.hooksPath=/dev/null"}, args...)
-
 	cmd := exec.CommandContext(ctx, gitPath, args...)
 
 	// Only set working directory for non-clone commands
 	// git clone creates the directory, so we can't cd into it first
-	if len(args) > 0 && args[2] != "clone" {
+	if len(args) > 0 && args[0] != "clone" {
 		cmd.Dir = r.CloneDir
 	}
 
